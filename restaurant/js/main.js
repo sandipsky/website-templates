64c1210267f1b53bd2296185a793c16b9ce92793
sandipsky <sandipshakya75@gmail.com>
2024-12-22 17:06:39 +0545

    changes

--- restaurant/js/main.js
+++ restaurant/js/main.js
@@ -1,6 +1,7 @@
 const mobileMenu = document.getElementById('mobile-menu');
 const navClose = document.getElementById('nav-close');
 const sideMenu = document.querySelector('.side-menu');
+const nav = document.querySelector('.navbar');
 
 mobileMenu.addEventListener('click', () => {
     sideMenu.classList.add('open');
@@ -8,4 +9,13 @@ mobileMenu.addEventListener('click', () => {
 
 navClose.addEventListener('click', () => {
     sideMenu.classList.remove('open');
+});
+
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
 });
