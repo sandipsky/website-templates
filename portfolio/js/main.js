b7d5fdc0ce19573e6e673769585635b99f175e52
sandipsky <sandipshakya75@gmail.com>
2024-12-27 17:46:12 +0545

    changes

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,8 +1,12 @@
-AOS.init();
+AOS.init({
+    duration: 700,
+    mirror: true,
+});
 const mobileMenu = document.getElementById('mobile-menu');
 const navClose = document.getElementById('nav-close');
 const sideMenu = document.querySelector('.side-menu');
 const nav = document.querySelector('.navbar');
+const navLinks = document.querySelectorAll('.nav-links a'); 
 
 mobileMenu.addEventListener('click', () => {
     sideMenu.classList.add('open');
@@ -12,6 +16,11 @@ navClose.addEventListener('click', () => {
     sideMenu.classList.remove('open');
 });
 
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        sideMenu.classList.remove('open');
+    });
+});
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 0) {
